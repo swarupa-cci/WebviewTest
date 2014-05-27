bd2326f49403f70d9d5baa70b7a1e630298461ea
Swarupa Vengurlekar <swarupa.vengurlekar@creativecapsule.com>
2014-05-27 15:01:24 +0530

    Test commit2

--- WebviewTest/src/com/example/webviewtest/MainActivity.java
+++ WebviewTest/src/com/example/webviewtest/MainActivity.java
@@ -24,6 +24,7 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
 	private ProgressDialog progressBar; 
 	private String test;
+	String test2;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
